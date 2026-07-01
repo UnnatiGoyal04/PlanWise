b38a784af104142e3b3a03e49f2f7a37be3d543a
Unnati Goyal <unnati4506@gmail.com>
2026-07-01 18:51:37 +0530

    Implement task validation and response models

--- backend/app/routers/task.py
+++ backend/app/routers/task.py
@@ -1,12 +1,22 @@
 from fastapi import APIRouter
-from app.schemas.task import TaskCreate
+from app.schemas.task import TaskCreate, TaskResponse
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
 
-@router.post("/")
+@router.post(
+    "/",
+    response_model=TaskResponse
+)
 def create_task(task: TaskCreate):
-    return {
-        "message": "Task created successfully!",
-        "task": task
-    }
+    return TaskResponse(
+        id=1,
+        title=task.title,
+        subject=task.subject,
+        description=task.description,
+        priority=task.priority,
+        estimated_hours=task.estimated_hours,
+        completed=task.completed
+    )
+        
+    
--- backend/app/schemas/task.py
+++ backend/app/schemas/task.py
@@ -1,9 +1,46 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from app.enums.priority import Priority
 
 class TaskCreate(BaseModel):
-    title:str
-    subject:str
-    description:str | None=None
-    priority:str
-    estimated_hours: int | None=None
-    completed:bool = False
+    title: str = Field(
+        ...,
+        min_length=3,
+        max_length=100,
+        description="Title of the study task"
+    )
+
+    subject: str = Field(
+        ...,
+        min_length=2,
+        max_length=50,
+        description="Subject name"
+    )
+
+    description: str | None = Field(
+        default=None,
+        max_length=500,
+        description="Optional description"
+    )
+
+    priority: Priority = Field(
+        ...,
+        description="Task priority"
+    )
+
+    estimated_hours: int | None = Field(
+        default=None,
+        ge=1,
+        le=100,
+        description="Estimated study hours"
+    )
+
+    completed: bool = False
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    subject: str
+    description: str | None
+    priority: Priority
+    estimated_hours: int | None
+    completed: bool
