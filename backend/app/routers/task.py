bdb748289f40cfa1b24094f9b479b766f33608ae
Unnati Goyal <unnati4506@gmail.com>
2026-07-05 00:03:50 +0530

    Phase 2.7 Complete - Read Task Endpoints

--- backend/app/routers/task.py
+++ backend/app/routers/task.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.database import get_db
 from app.schemas.task import TaskCreate, TaskResponse
 from app.models.task import Task
+
+from sqlalchemy import select
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
@@ -37,3 +39,32 @@ async def create_task(
     estimated_hours=db_task.estimated_hours,
     completed=db_task.completed
 )
+@router.get(
+    "/tasks",
+    response_model=list[TaskResponse]
+)
+async def get_tasks(
+    db: AsyncSession = Depends(get_db)
+):
+    query=select(Task)
+    result=await db.execute(query)
+    tasks= result.scalars().all()
+    return tasks
+@router.get(
+    "/tasks/{id}",
+    response_model=TaskResponse
+)    
+async def get_task(
+    id:int,
+    db:AsyncSession=Depends(get_db)
+):
+    query = select(Task).where(Task.id == id)
+    result= await db.execute(query)
+    task = result.scalar_one_or_none()
+
+    if task is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Task not found"
+        )
+    return task
